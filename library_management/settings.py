21693bb21f24acc94c525472cf6ba47922b507f5
Nohan-Ahmed <mdpolashislam152@gmail.com>
2024-07-04 16:37:44 +0600

    Create virtual environment

--- library_management/settings.py
+++ library_management/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&q7e-zld=xl#oy#+avkg9c^e29fpyu5euuicja7i06b9d73c-a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
